9a46195ae175c215e5e2ad796e931e46b0530bfc
toualbiaharoun <toualbiaharoun@gmail.com>
2025-12-25 18:54:11 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+print("welcome ".title())
+first_num = float(input("enter the first number ".title()))
+second_num = float(input("enter the second number ".title()))
+print("____________________________")
+adding = first_num + second_num
+print("adding = ".title() + str(adding))
+subtract = first_num - second_num
+print("subtract = ".title() + str(subtract))
+multiply = first_num * second_num
+print("multiply = ".title() + str(multiply))
+divide = first_num / second_num
+print("divide = ".title() + str(divide))
+modulus = first_num % second_num
+print("modulus = ".title() +  str(modulus))
